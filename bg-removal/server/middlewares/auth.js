08f39f9a7385826ba16c3345e7764d090671f404
hema-sindhu <sindhu6102@gmail.com>
2024-10-26 18:41:15 +0530

    Add files via upload

---
+++ bg-removal/server/middlewares/auth.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken'
+
+const authUser = async (req, res, next) => {
+
+    try {
+
+        // getting token from headers
+        const { token } = req.headers;
+
+        // checking token availability
+        if (!token) {
+            return res.json({ success: false, message: 'Not Authorized Login Again' })
+        }
+
+        // decoding JWT token
+        const token_decode = jwt.decode(token)
+
+        // getting clerkId from decoded token
+        req.body.clerkId = token_decode.clerkId
+        next()
+
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+    }
+
+}
+
+export default authUser
